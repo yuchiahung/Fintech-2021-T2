2b3596d6fb55654431271096996afd91861f7f67
ana hung <b05303007@ntu.edu.tw>
2021-06-11 00:57:50 +0800

    Update summarized_news.py

--- summarized_news.py
+++ summarized_news.py
@@ -1,5 +1,5 @@
-from summarizer import Summarizer
-from transformers import AutoTokenizer, AutoConfig, AutoModel
+from gensim.summarization.summarizer import summarize
+# from gensim.summarization.textcleaner import split_sentences
 import re
 
 # extract selected company's news header
@@ -7,16 +7,15 @@ def summarized_multiple_news(company_news, n_sen):
     # replace \. with space
     # sort by time (latest news first)
     company_news.sort_values(by = 'time', ascending = False)
-    company_news.loc[:, 'header_clean'] = [re.sub('\.', ' ', h) for h in company_news.header].copy()
-    company_headers = '. '.join(company_news['header_clean'].tolist())
+    company_news.loc[:, 'header_clean'] = [re.sub('\.|\?', ' ', h) for h in company_news.header].copy()
+    company_headers = company_news['header_clean'].tolist()
+    company_headers_all = '\n'.join(company_headers)
     # summaize headers
-    modelName = "bert-base-uncased" # lower-cased
-    custom_config = AutoConfig.from_pretrained(modelName)
-    custom_config.output_hidden_states=True
-    custom_tokenizer = AutoTokenizer.from_pretrained(modelName)
-    custom_model = AutoModel.from_pretrained(modelName, config=custom_config)
-    model = Summarizer(custom_model=custom_model, custom_tokenizer=custom_tokenizer)
-    summarized_headers = model(company_headers, num_sentences = n_sen)[:-1].split('. ')
+    rt = n_sen / len(company_news)
+    summarized_headers = summarize(company_headers_all, ratio = rt, split = True)
+    
+    if len(summarized_headers) == 0:
+        summarized_headers = company_headers_all.copy()
     summarized_news_df = company_news[company_news.header_clean.isin(summarized_headers)]
     summarized_news_df.drop_duplicates(subset = ['header', 'header_clean'],inplace = True)
     return summarized_news_df
